0b6ea9c42d81fe36648ff098090790d64adec20e
Dilkash <dilkashpeshimam@gmail.com>
2022-10-17 13:57:46 +0530

    create main body & footer folder

--- client/src/components/MainBody/MainBody.tsx
+++ client/src/components/MainBody/MainBody.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './MainBody.css'
 import Header from './MainBodyHeader/Header'
 import Divider from '@mui/material/Divider';
+import ChatSection from './MainBodySection/ChatSection';
+import Footer from './MainBodyFooter/Footer';
 
 
 const MainBody = () => {
@@ -9,6 +11,10 @@ const MainBody = () => {
     <div className='mainbody'>
       <Header />
       <Divider variant='inset' />
+      <ChatSection />
+      <Divider variant='middle' />
+
+      <Footer />
 
     </div>
   )
---
+++ client/src/components/MainBody/MainBodySection/ChatSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ChatSection.css'
+
+const ChatSection = () => {
+  return (
+    <div className='chat-section'>
+
+    </div>
+  )
+}
+
+export default ChatSection
