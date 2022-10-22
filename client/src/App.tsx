aaedf3e8c03e5b2505b4d774b0211e93bff13e9e
Dilkash <dilkashpeshimam@gmail.com>
2022-10-22 14:47:16 +0530

    created routes for home, login & registration page using react router dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.css';
 import Home from './components/HomePage/Home';
+import { Route, Routes } from 'react-router-dom'
+import Login from './components/LoginPage/Login';
+import Register from './components/RegisterPage/Register';
+
 
 
 function App() {
   return (
     <div className="app">
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
     </div>
+
+
   );
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,15 +6,14 @@ import './index.css'
 import { BrowserRouter } from "react-router-dom"
 
 
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-
     </BrowserRouter>
   </React.StrictMode>
 );
