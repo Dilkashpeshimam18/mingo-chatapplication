40082fd7df1bc6d1fc34d03f264c7dcc807774cd
Dilkash <dilkashpeshimam@gmail.com>
2022-10-22 16:49:17 +0530

    created assets folder & added illustration on register page

--- client/src/components/RegisterPage/Register.tsx
+++ client/src/components/RegisterPage/Register.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './Register.css'
+// import GroupChatImg from '../assets/GroupChat.gif'
+const GroupChatImg =  require("../assets/GroupChat.gif");
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='register'>
+        <div className='register-left'>
+            <div className='img-container'>
+            <img style={{height:'650px', width:'650px'}} src={GroupChatImg} />
+
+            </div>
+        </div>
+        <div className='register-right'>
+
+        </div>
+
+    </div>
   )
 }
 
