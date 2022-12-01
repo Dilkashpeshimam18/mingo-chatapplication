8a51c818e4c2f9e5f7f54266fc00de6c953af6a9
Dilkash <dilkashpeshimam@gmail.com>
2022-12-01 17:14:01 +0530

    created socket server & coonected to frontend

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const http = require('http')
+const cors = require('cors')
+const { Server } = require('socket.io')
+
+const app = express()
+//middleware
+app.use(cors())
+
+//creating http server of our app
+
+const server = http.createServer(app)
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello'
+    })
+})
+//creating instance of socket.io
+const io = new Server(server, {
+    cors: {
+        origin: 'https://localhost:3000',
+        methods: ["GET", "POST"],
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log(socket.id)
+
+    socket.on('disconnect', () => {
+        console.log('user discoonect', socket.id)
+    })
+})
+
+server.listen(3001, () => {
+    console.log('SERVER RUNNING')
+})
+
