800d134e1dd4c2a9dc88cee2e5adf3b99f74dcd2
Dilkash <dilkashpeshimam@gmail.com>
2022-12-22 14:24:17 +0530

    created authReducer & added to store

---
+++ client/src/store/slice/authSlice.tsx
@@ -0,0 +1,42 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+
+export type AuthType = {
+    isAuthenticated: boolean,
+    user: {
+        name: string | null,
+        email: string | null,
+        photoUrl: string | null,
+        bio: string | null,
+        accessToken: string | null,
+        uid: string | null,
+
+    }
+}
+
+const initialAuthState: AuthType = {
+    isAuthenticated: false,
+    user: {
+        name: '',
+        email: '',
+        photoUrl: '',
+        bio: '',
+        accessToken: null,
+        uid: null
+    }
+}
+
+const AuthSlice = createSlice({
+    name: 'auth',
+    initialState: initialAuthState,
+    reducers: {
+        addUserDetail(state, action: PayloadAction<any>) {
+            state.user = action.payload
+            state.isAuthenticated = true
+        }
+
+    }
+})
+
+export const authActions = AuthSlice.actions
+export default AuthSlice.reducer
--- client/src/store/store.tsx
+++ client/src/store/store.tsx
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from './slice/authSlice'
 
 const store = configureStore({
     reducer: {
-
+        auth: authReducer
     }
 })
 
 export default store
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
