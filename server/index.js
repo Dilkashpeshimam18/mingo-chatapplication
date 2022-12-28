9934c903cf40475cac8ce553f712832fede11228
Dilkash <dilkashpeshimam@gmail.com>
2022-12-28 21:07:21 +0530

    created join room & send message functionality in socket

--- server/index.js
+++ server/index.js
@@ -27,7 +27,12 @@ const io = new Server(server, {
 
 io.on('connection', (socket) => {
     console.log(`User connected ${socket.id}`);
-
+    socket.on('join_room', (data) => {
+        socket.join(data)
+    })
+    socket.on('send_message', (data) => {
+        socket.to(data.room).emit('receive_message', data)
+    })
     socket.on('disconnect', () => {
         console.log('user discoonect', socket.id)
     })
