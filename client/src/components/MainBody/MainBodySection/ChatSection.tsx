90dfa316ae98cfe3f5005b18a1f9eaf14c3a6dbc
Dilkash Peshimam <dilkashpeshimam@gmail.com>
2023-08-07 12:54:56 +0530

    fix pdf repetitive download

--- client/src/components/MainBody/MainBodySection/ChatSection.tsx
+++ client/src/components/MainBody/MainBodySection/ChatSection.tsx
@@ -1,20 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import './ChatSection.css'
 import Message from './Message'
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../store/store';
 import Avatar from '@mui/material/Avatar';
 
-
 const ChatSection = () => {
   const allMessage = useSelector((state: RootState) => state.message.allMessages)
   const user = useSelector((state: RootState) => state.auth.user)
 
-  const renderFilePreview = (fileUrl: string, name: string, image: string, email: string) => {
+  const renderFilePreview =(fileUrl: string, name: string, image: string, email: string) => {
     const fileType = fileUrl?.split('.').pop()?.toLowerCase();
 
     if (fileType) {
-      if (['png', 'jpg', 'jpeg', 'gif','avif','webp'].includes(fileType)) {
+      if (['png', 'jpg', 'jpeg', 'gif', 'avif', 'webp'].includes(fileType)) {
         return (
           <div className='message--container '>
             <div className='message--avatar'>
@@ -56,20 +55,20 @@ const ChatSection = () => {
         return (
           <div className='message--container '>
             <div className='message--avatar'>
-              <Avatar src={image == null ? (
+              <Avatar 
+              src={image == null ? (
                 user.email == email ? user.photoUrl as string : image
-              ) : (image)} sx={{ width: 45, height: 45 }} />
+              ) : (image)}
+               sx={{ width: 45, height: 45 }} />
             </div>
             <div className='message-subContainer '>
               <div className='message-user'>
                 <span className='sender-name'>{name}</span>
-
               </div>
               <div className={user.name == name ? 'message-sender' : 'message-body'} >
-                <p>Pdf file downloaded</p>
-                <embed style={{ borderRadius: '5px', marginTop: '3px' }} src={fileUrl} width="120" height="3px" type="application/pdf" />              </div>
+                <a href={fileUrl} style={{fontSize:'12px'}} target="_blank" rel="noopener noreferrer">Download PDF</a>
+              </div>
             </div>
-
           </div>
         );
       } else {
@@ -86,7 +85,7 @@ const ChatSection = () => {
 
               </div>
               <div >
-                <p style={{fontSize:'smaller'}}>Unsupported file type: {fileType}</p>              </div>
+                <p style={{ fontSize: 'smaller' }}>Unsupported file type: {fileType}</p>              </div>
             </div>
 
           </div>
@@ -96,6 +95,7 @@ const ChatSection = () => {
       return null; // or return a fallback UI if fileUrl is undefined
     }
   };
+
   return (
     <div className='chat-body'>
       <div className='chat-section'>
--- server/controllers/message.js
+++ server/controllers/message.js
@@ -184,3 +184,4 @@ exports.getUploadedFiles = async (req, res) => {
     }
 }
 
+
