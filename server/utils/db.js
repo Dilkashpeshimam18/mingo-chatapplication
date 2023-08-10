b676a9c084b957f24127b2780106e2fc0fd57ee3
Dilkash Peshimam <dilkashpeshimam@gmail.com>
2023-08-10 15:12:38 +0530

    changes

--- server/utils/db.js
+++ server/utils/db.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: 'localhost',
+    host: process.env.DB_HOST,
     dialect: 'mysql'
 })
 
